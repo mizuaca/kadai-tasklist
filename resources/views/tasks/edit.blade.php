2402d599ad98fd70d8640101e2c2db3275e46c91
mizuaca <mizuaca@gmail.com>
2017-12-12 07:46:07 +0000

    modified grid

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,9 +6,10 @@
 
 <div class="row">
   <div class="
+    col-xs-12
     col-sm-offset-2 col-sm-8 
     col-md-offset-2 col-md-8 
-    col-lg-offset-3 col-lg-5"> 
+    col-lg-offset-3 col-lg-6"> 
     {!! Form::model($task, ['route' => ['tasks.update',$task->id],'method' => 'put']) !!}
       <div class="form-group">
         {!! Form::label('status','実行状況:') !!}
